1e16f8fc703ad4d3f20d6752b347ff54cb4b91be
dereypl <mpasich.pk@gmail.com>
2019-04-10 01:01:33 +0200

    add array with clients and oferta -> ShowReservationDetails not works

--- src/main/java/Szrotex3/ui/klienci/KlienciController.java
+++ src/main/java/Szrotex3/ui/klienci/KlienciController.java
@@ -1,7 +1,33 @@
 package Szrotex3.ui.klienci;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
 
-public class KlienciController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class KlienciController implements Initializable {
+
+    @FXML
+    private VBox client_container;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Node[] nodes = new Node[10];
+        for (int i = 0; i < nodes.length; i++) {
+
+            try {
+                nodes[i] = FXMLLoader.load(getClass().getResource("klient.fxml"));
+                client_container.getChildren().add(nodes[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
--- src/main/java/Szrotex3/ui/oferta/OfertaController.java
+++ src/main/java/Szrotex3/ui/oferta/OfertaController.java
@@ -4,24 +4,50 @@ import Szrotex3.ui.homepage.HomePageController;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class OfertaController  {
+public class OfertaController implements Initializable {
 
 
     @FXML
     private AnchorPane oferta_content_pane;
 
-    @FXML
-    private JFXButton ReserveButton;
+//   @FXML
+//    private JFXButton ReserveButton;
 
     @FXML
-    void ShowReservationDetails(ActionEvent event) {
-        oferta_content_pane.getChildren().clear();
-        HomePageController.getInstance().createContentPage(oferta_content_pane,"/Szrotex3/ui/makereservation/content_makereservation.fxml");
-        HomePageController.getInstance().setTopPath("Szczegóły rezerwacji");
+    private VBox container_oferta;
+
+//    @FXML
+//    void ShowReservationDetails(ActionEvent event) {
+//        oferta_content_pane.getChildren().clear();
+//        HomePageController.getInstance().createContentPage(oferta_content_pane,"/Szrotex3/ui/makereservation/content_makereservation.fxml");
+//        HomePageController.getInstance().setTopPath("Szczegóły rezerwacji");
+//    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Node[] cars = new Node[10];
+        for (int i = 0; i < cars.length; i++) {
+
+            try {
+                cars[i] = FXMLLoader.load(getClass().getResource("one_car_pane.fxml"));
+                container_oferta.getChildren().add(cars[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 
 }
