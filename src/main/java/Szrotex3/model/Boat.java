de31d479a8bdb6007cd52ebcb947109b39feb1ea
MrWonsik <46217263+MrWonsik@users.noreply.github.com>
2019-04-09 18:26:25 +0200

    Dodanie katalogu test + klasy boat z brancha pomFix

---
+++ src/main/java/Szrotex3/model/Boat.java
@@ -0,0 +1,50 @@
+package Szrotex3.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "boat")
+public class Boat {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @PrimaryKeyJoinColumn
+    @OneToOne(cascade = CascadeType.ALL, optional = false)
+    private Vehicle vehicle;
+
+    @Column(name = "displacement")
+    private int displacement;
+
+    public Boat() {
+
+    }
+
+    public Boat(Vehicle vehicle, int displacement) {
+        this.setVehicle(vehicle);
+        this.displacement = displacement;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(int displacement) {
+        this.displacement = displacement;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.id = vehicle.getId();
+        this.vehicle = vehicle;
+    }
+
+}
---
+++ src/test/java/Szrotex3/Integration/OneToOneTest.java
@@ -0,0 +1,53 @@
+package Szrotex3.model;
+
+import Szrotex3.model.Boat;
+import Szrotex3.model.Vehicle;
+import Szrotex3.service.Container;
+import Szrotex3.service.HibernateSession;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OneToOneTest {
+
+    @Test
+    public void vehicleBoatOneToOneTest() {
+
+        HibernateSession hibernateSession = (HibernateSession) Container.getBean("hibernateSession");
+
+        Vehicle newVehicle = new Vehicle("Maxus 33", 42, "black");
+        hibernateSession.getSession().persist(newVehicle);
+
+        Boat newBoat = new Boat(newVehicle,6201);
+
+        hibernateSession.getSession().persist(newBoat);
+        hibernateSession.getSession().flush();
+
+        int boatId = newBoat.getId();
+
+        Boat fetchedBoat = (Boat)  hibernateSession.getSession().get(Boat.class, boatId);
+
+        assertNotNull(fetchedBoat);
+
+        assertEquals(newBoat.getDisplacement(), fetchedBoat.getDisplacement());
+
+        Vehicle fetchedVehicle = fetchedBoat.getVehicle();
+
+        assertNotNull(fetchedVehicle);
+
+        assertEquals(newVehicle.getName(), fetchedVehicle.getName());
+
+        int vehicleId = fetchedVehicle.getId();
+
+        hibernateSession.getSession().delete(fetchedBoat);
+        hibernateSession.getSession().flush();
+
+        Boat deletedBoat = (Boat)  hibernateSession.getSession().get(Boat.class, boatId);
+        Vehicle deletedVehicle = (Vehicle)  hibernateSession.getSession().get(Vehicle.class, vehicleId);
+
+        assertNull(deletedBoat);
+        assertNull(deletedVehicle);
+
+    }
+
+}
