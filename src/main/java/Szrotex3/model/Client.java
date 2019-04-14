3a5ad2fe3f5eb275937fbc013e11269441bb4ce8
MrWonsik <46217263+MrWonsik@users.noreply.github.com>
2019-04-14 17:09:05 +0200

    Edycja klasy Client

--- src/main/java/Szrotex3/model/Client.java
+++ src/main/java/Szrotex3/model/Client.java
@@ -30,7 +30,26 @@ public class Client {
     @CreationTimestamp
     private Date createdAt;
 
+    @Column(name="pesel")
+    private String Pesel;
 
+    @Column(name="id_number")
+    private String idNumber;
+
+    @Column(name="birth_date")
+    private Date birthDate;
+
+    @Column(name="city")
+    private String city;
+
+    @Column(name="street")
+    private String street;
+
+    @Column(name="apartment_number")
+    private String apartmentNumber;
+
+    @Column(name="postal_code")
+    private String postalCode;
 
     public Client()
     {
@@ -93,5 +112,59 @@ public class Client {
         this.id = id;
     }
 
+    public String getPesel() {
+        return Pesel;
+    }
 
+    public void setPesel(String pesel) {
+        Pesel = pesel;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
