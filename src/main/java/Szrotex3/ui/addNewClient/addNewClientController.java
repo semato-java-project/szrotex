d6680de043e6bccda766556d7a4578648fca2153
dereypl <mpasich.pk@gmail.com>
2019-04-14 18:57:13 +0200

    AddNewClient done.

--- src/main/java/Szrotex3/ui/addNewClient/addNewClientController.java
+++ src/main/java/Szrotex3/ui/addNewClient/addNewClientController.java
@@ -1,10 +1,17 @@
 package Szrotex3.ui.addNewClient;
 
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class addNewClientController {
+
+    @FXML
+    private AnchorPane AddClientPane;
 
     @FXML
     private JFXTextField ClientName;
