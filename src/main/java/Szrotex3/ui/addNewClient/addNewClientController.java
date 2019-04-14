7ba04daa6ed90c0982a77f1989f75fda9b7efe42
MrWonsik <46217263+MrWonsik@users.noreply.github.com>
2019-04-14 19:51:19 +0200

    Przymiarki do formularza klienta

--- src/main/java/Szrotex3/ui/addNewClient/addNewClientController.java
+++ src/main/java/Szrotex3/ui/addNewClient/addNewClientController.java
@@ -1,5 +1,6 @@
 package Szrotex3.ui.addNewClient;
 
+import Szrotex3.model.Client;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXTextField;
@@ -14,31 +15,41 @@ public class addNewClientController {
     private AnchorPane AddClientPane;
 
     @FXML
-    private JFXTextField ClientName;
+    private JFXTextField clientName;
 
     @FXML
-    private JFXTextField ClientSurname;
+    private JFXTextField clientSurname;
 
     @FXML
-    private JFXTextField ClientSurname1;
+    private JFXTextField clientPhone;
 
     @FXML
-    private JFXTextField ClientSurname11;
+    private JFXTextField clientEmail;
 
     @FXML
-    private JFXTextField ClientName1;
+    private JFXTextField clientPesel;
 
     @FXML
-    private JFXTextField ClientSurname2;
+    private JFXTextField clientIdNumber;
 
     @FXML
-    private JFXTextField ClientSurname12;
+    private JFXTextField clientBirthDate;
 
     @FXML
-    private JFXTextField ClientSurname111;
+    private JFXTextField clientCity;
+
+    @FXML
+    private JFXTextField clientStreet;
+
+    @FXML
+    private JFXTextField clientApartmentNumber;
+
+    @FXML
+    private JFXTextField clientPostalCode;
 
     @FXML
     void handleAddClientAction(ActionEvent event) {
+        Client newClient(clientName.getText(), clientSurname.getText(), clientEmail.getText(), clientPhone.getText(), clientPesel.getText(), clientIdNumber.getText(), clientBirthDate.getText(), clientCity.getText(), clientStreet.getText(), clientApartmentNumber.getText(), clientPostalCode.getText());
 
     }
 
