9decd89c73f2463c136b6eded582950f76d5e450
MrWonsik <46217263+MrWonsik@users.noreply.github.com>
2019-04-15 21:32:02 +0200

    Walidacja w formularzu

--- src/main/java/Szrotex3/ui/addNewClient/addNewClientController.java
+++ src/main/java/Szrotex3/ui/addNewClient/addNewClientController.java
@@ -4,6 +4,7 @@ import Szrotex3.model.Client;
 import Szrotex3.service.Container;
 import Szrotex3.service.HibernateSession;
 import Szrotex3.ui.homepage.HomePageController;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
@@ -14,8 +15,21 @@ import javafx.scene.layout.AnchorPane;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class addNewClientController {
+
+    private static addNewClientController instance;
+
+    public addNewClientController(){
+        instance = this;
+    }
+    public static addNewClientController getInstance(){
+        return instance;
+    }
+
+    private Client client;
 
     @FXML
     private AnchorPane AddClientPane;
@@ -54,6 +68,11 @@ public class addNewClientController {
     private JFXTextField clientPostalCode;
 
     @FXML
+    private JFXButton addOrEditButton;
+
+
+
+    @FXML
     void handleAddClientAction(ActionEvent event) {
         String firstName = clientName.getText();
         String lastName = clientSurname.getText();
@@ -67,27 +86,37 @@ public class addNewClientController {
         String apartmentNumber = clientApartmentNumber.getText();
         String postalCode = clientPostalCode.getText();
 
-        Calendar now= Calendar.getInstance();
-        Date createAt = now.getTime();
+        if(localBirthDate != null) {
+            Calendar now = Calendar.getInstance();
+            Date createAt = now.getTime();
 
-        Calendar calendarBirthDate=Calendar.getInstance();
+            Calendar calendarBirthDate = Calendar.getInstance();
 
-        calendarBirthDate.set(
-                localBirthDate.getYear(),
-                localBirthDate.getMonthValue() - 1,
-                localBirthDate.getDayOfMonth()
-        );
+            calendarBirthDate.set(
+                    localBirthDate.getYear(),
+                    localBirthDate.getMonthValue() - 1,
+                    localBirthDate.getDayOfMonth()
+            );
 
-        Date birthDate = calendarBirthDate.getTime();
+            Date birthDate = calendarBirthDate.getTime();
 
-        Client newClient=new Client(firstName, lastName, email, phone, createAt, pesel, idNumber, birthDate, city, street, apartmentNumber, postalCode);
+            Client newClient = new Client(firstName, lastName, email, phone, createAt, pesel, idNumber, birthDate, city, street, apartmentNumber, postalCode);
 
-        HibernateSession hibernateSession = (HibernateSession) Container.getBean("hibernateSession");
+            if (addNewClientController.getInstance().validate(newClient)) {
 
-        hibernateSession.getSession().persist(newClient);
-        hibernateSession.getSession().flush();
+                HibernateSession hibernateSession = (HibernateSession) Container.getBean("hibernateSession");
 
-        //prompt o dodaniu nowego klienta, wymazanie pol.
+                hibernateSession.getSession().persist(newClient);
+                hibernateSession.getSession().flush();
+
+                //prompt o dodaniu nowego klienta, wymazanie pol.
+            }
+        }
+        else
+        {
+            System.out.println("Wprowadź datę urodzenia!");
+        }
+
 
         //HomePageController.getInstance().changeContentToKlienci(event);
 
@@ -97,6 +126,227 @@ public class addNewClientController {
 
 
     }
+
+
+    private boolean validate(Client clientToValidate)
+    {
+        boolean validation=true;
+        String ifEmpty="Pole nie może być puste!";
+
+        if(clientToValidate.getFirstName().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+
+        if(clientToValidate.getLastName().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+
+        if(clientToValidate.getEmail().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+        else
+        {
+            Pattern mailPattern = Pattern.compile(
+                    "^[a-zA-z0-9]+[\\._a-zA-Z0-9]*@[a-zA-Z0-9]" +
+                            "+{2,}\\.[a-zA-Z]{2,}[\\.a-zA-Z0-9]*$");
+
+            Matcher mailMatcher = mailPattern.matcher(clientToValidate.getEmail());
+
+            if(!mailMatcher.matches())
+            {
+                System.out.println("Wprowadzono nie poprawny mail!");
+            }
+
+        }
+
+        if(clientToValidate.getPhone().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+        else if(clientToValidate.getPhone().length() != 9)
+        {
+            System.out.println("Dlugość telefonu musi wynosić 9 znaków");
+            validation=false;
+        }
+
+        if(clientToValidate.getPesel().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+        else if(clientToValidate.getPesel().length() != 11)
+        {
+            System.out.println("Pesel ma 11 znaków!");
+            validation=false;
+        }
+        else
+        {
+            String pesel=clientToValidate.getPesel();
+            char[] peselChars = new char[pesel.length()];
+            pesel.getChars(0,pesel.length(), peselChars, 0);
+            boolean validationPesel = true;
+            for(int i=0 ; i<pesel.length();i++)
+            {
+                if(!Character.isDigit(peselChars[i]))
+                {
+                    validationPesel = false;
+                }
+            }
+
+            if(!validationPesel) {
+                System.out.println("Użyto nie poprawnych znaków!");
+                validation = false;
+            }
+        }
+
+        if(clientToValidate.getIdNumber().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+        else if(clientToValidate.getIdNumber().length() != 9)
+        {
+            System.out.println("Numer dowodu ma 9 znaków!");
+            validation=false;
+        }
+        else
+        {
+            String idNumber=clientToValidate.getIdNumber();
+            char[] idNumberChars = new char[idNumber.length()];
+            idNumber.getChars(0,idNumber.length(), idNumberChars, 0);
+            boolean validationIdNumber = true;
+            for(int i=0;i<2;i++)
+            {
+                if(!Character.isLetter(idNumberChars[i]))
+                {
+                    validationIdNumber=false;
+                }
+            }
+
+            for(int i=3;i<idNumber.length();i++)
+            {
+                if(!Character.isDigit(idNumberChars[i]))
+                {
+                    validationIdNumber=false;
+                }
+            }
+
+            if(!validationIdNumber)
+            {
+                System.out.println("Niepoprawny numer dowodu!");
+                validation=false;
+            }
+        }
+
+        if(clientToValidate.getBirthDate() == null)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+        else
+        {
+            Calendar now= Calendar.getInstance();
+            Date today = now.getTime();
+
+            Calendar calendarBirthDate=Calendar.getInstance();
+
+            if(clientToValidate.getBirthDate().after(today))
+            {
+                System.out.println("Wprowadzono niepoprawną datę!");
+                validation=false;
+            }
+        }
+
+        if(clientToValidate.getCity().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+
+        if(clientToValidate.getStreet().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+
+        if(clientToValidate.getApartmentNumber().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+
+
+        if(clientToValidate.getPostalCode().length() == 0)
+        {
+            System.out.println(ifEmpty);
+            validation=false;
+        }
+        else
+        {
+            String postalCode=clientToValidate.getPostalCode();
+            char[] postalCodeChars = new char[postalCode.length()];
+            postalCode.getChars(0,postalCode.length(), postalCodeChars, 0);
+            boolean validationPostalCode = true;
+            for(int i=0;i<1;i++)
+            {
+                if(!Character.isDigit(postalCodeChars[i]))
+                {
+                    validationPostalCode=false;
+                }
+            }
+
+            if(postalCodeChars[2]!='-')
+            {
+                validationPostalCode=false;
+            }
+
+            for(int i=3;i<postalCode.length();i++)
+            {
+                if(!Character.isDigit(postalCodeChars[i]))
+                {
+                    validationPostalCode=false;
+                }
+            }
+
+            if(!validationPostalCode)
+            {
+                System.out.println("Niepoprawny kod pocztowy!");
+                validation=false;
+            }
+        }
+
+        return validation;
+    }
+
+
+    public JFXButton getAddOrEditButton() {
+        return addOrEditButton;
+    }
+
+    public void setClient(Client client)
+    {
+        this.client=client;
+
+        clientName.setText(client.getFirstName());
+        clientSurname.setText(client.getLastName());
+        clientEmail.setText(client.getEmail());
+        clientPhone.setText(client.getPhone());
+        clientPesel.setText(client.getPesel());
+        clientIdNumber.setText(client.getIdNumber());
+        //aclientBirthDate.setText(String.valueOf(client.getBirthDate()));
+        clientStreet.setText(client.getStreet());
+        clientCity.setText(client.getCity());
+        clientApartmentNumber.setText(client.getApartmentNumber());
+        clientPostalCode.setText(client.getPostalCode());
+    }
+
 
     @FXML
     void handleCancelAction(ActionEvent event) {
