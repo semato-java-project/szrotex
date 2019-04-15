5a3ae2e13c9309d2dc6561cfa656effa4daaede3
dereypl <mpasich.pk@gmail.com>
2019-04-15 23:07:58 +0200

    Add labels inside oneRentController

--- src/main/java/Szrotex3/ui/rentlist/OneRentController.java
+++ src/main/java/Szrotex3/ui/rentlist/OneRentController.java
@@ -1,6 +1,42 @@
 package Szrotex3.ui.rentlist;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class OneRentController {
+
+    @FXML
+    private Text carBrand;
+
+    @FXML
+    private Text carModel;
+
+    @FXML
+    private Text dateStart;
+
+    @FXML
+    private Text dateEnd;
+
+    @FXML
+    private Text rentPrice;
+
+    @FXML
+    private Text Price1;
+
+    @FXML
+    private Text carId;
+
+    @FXML
+    private Text clientId;
+
+    @FXML
+    private Text clientName;
+
+    @FXML
+    private Text clientSurname;
+
+    @FXML
+    private Text rentId;
 
 
     private static OneRentController instance;
